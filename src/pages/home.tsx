f031b6cdb950717ebd9f5a0fac2a071eb4694d72
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-03-20 09:38:06 +0100

    updated project name, desc and dependencies

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import { GrammarlyEditorPlugin } from '@grammarly/editor-sdk-react'
+import { BeakerIcon } from '@heroicons/react/24/solid'
 
-export default function GrammarlyEditor() {
+export default function MyComponent() {
     return (
-        <GrammarlyEditorPlugin clientId="client_22ajvnNXjzUtbQddqHLtjC">
-            <textarea />
-        </GrammarlyEditorPlugin>
+        <div>
+            <BeakerIcon className="h-6 w-6 text-blue-500" />
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, dolorum repellat debitis officiis facilis culpa itaque accusantium perspiciatis saepe recusandae, officia quis doloremque illum dolores voluptatum sed ullam consequatur. Quis?
+            </p>
+        </div>
     )
 }
