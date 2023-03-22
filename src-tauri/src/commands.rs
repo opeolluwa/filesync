80aecc4b60adb42171f0fb02ee2f1f9b23990ef6
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-03-22 18:54:43 +0100

    tried parsing file meta data

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,12 +1,15 @@
 // use glob::glob;
 use local_ip_address::local_ip;
 use serde::{Deserialize, Serialize};
-use std::{fs, path::PathBuf};
+use std::{
+    fmt, fs,
+    path::{Path, PathBuf},
+};
 extern crate dirs;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CommandData<T> {
-    data: Option<T>,
+    pub data: Option<T>,
     message: String,
     status: bool,
 }
@@ -41,10 +44,38 @@ pub fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+// the audio file interface
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct AudioFile {
+    name: String,
+    format: String,
+    path: PathBuf,
+}
+// AudioFile constructor
+impl AudioFile {
+    pub fn new(name: &str, format: &str, path: PathBuf) -> Self {
+        Self {
+            name: name.to_string(),
+            format: format.to_string(),
+            path,
+        }
+    }
+}
+// implement display for the AudioFiles
+impl fmt::Display for AudioFile {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "(name: {}\nformat: {}\npath: {:?})",
+            self.name, self.format, self.path
+        )
+    }
+}
+
 // get the audio file form the default audio dir of the OS
 // return an instance of the CommandData and vector of the path if any
 #[tauri::command]
-pub fn fetch_audio_files() -> Result<CommandData<Vec<PathBuf>>, CommandData<()>> {
+pub fn fetch_audio_files() -> Result<CommandData<Vec<AudioFile>>, CommandData<()>> {
     let audio_dir = dirs::audio_dir();
 
     // if there is an error getting the audio path, fire an error
@@ -53,10 +84,15 @@ pub fn fetch_audio_files() -> Result<CommandData<Vec<PathBuf>>, CommandData<()>>
     };
 
     //
-    let mut entries: Vec<PathBuf> = vec![];
+    let mut entries: Vec<AudioFile> = vec![];
     for entry in fs::read_dir(audio_dir).expect("error reading file") {
         let dir = entry.expect("could not read dir");
-        entries.push(dir.path());
+        let file = &dir.path();
+        let file_name = Path::new(file).file_name().unwrap().to_str().unwrap();
+        let file_extension = Path::new(file).extension().unwrap().to_str().unwrap();
+        let file_path = &dir.path();
+        let audio_file = AudioFile::new(file_name, file_extension, file_path.to_path_buf());
+        entries.push(audio_file);
     }
     Ok(CommandData::new("retrieved all audio files", true, entries))
 }
@@ -84,17 +120,17 @@ pub fn fetch_video_files() -> Result<CommandData<Vec<PathBuf>>, CommandData<()>>
 
 #[cfg(test)]
 mod tests {
-    use crate::commands;
+    use crate::commands::{self, AudioFile};
 
-    #[test]// see if there are files in the audio directory path
+    #[test] // see if there are files in the audio directory path
     fn _fetch_audio_files_() {
         let aud_files = commands::fetch_audio_files().ok();
         assert!(aud_files.is_some())
     }
 
-    #[test]// see if there are files in the video directory path
+    #[test] // see if there are files in the video directory path
     fn _fetch_video_files_() {
-        let vid_files: Option<commands::CommandData<Vec<std::path::PathBuf>>> =
+        let vid_files: Option<commands::CommandData<Vec<AudioFile>>> =
             commands::fetch_audio_files().ok();
         assert!(vid_files.is_some())
     }
