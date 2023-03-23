678a4ad6b9b2c5df66b52390b9d76367f93618fc
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-03-23 12:23:04 +0100

    configured splashscreen ui

--- src/components/AppNavigation.tsx
+++ src/components/AppNavigation.tsx
@@ -1,4 +1,4 @@
-import Home from '@/pages/splashscreen'
+import Home from '@/pages/home'
 import { Cog8ToothIcon, HomeIcon, FolderOpenIcon, WifiIcon, ArrowRightOnRectangleIcon, InformationCircleIcon } from '@heroicons/react/24/outline'
 import AppLogo from './AppLogo'
 import Link from 'next/link'
