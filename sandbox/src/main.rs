e12020b030ae9dd7b2e624f0e7e0cbede226a4c6
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-03-31 10:33:20 +0100

    sandbox - > configured file server to recieve file

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,3 +1,4 @@
+use axum::extract::DefaultBodyLimit;
 use axum::extract::Multipart;
 use axum::http::StatusCode;
 use axum::response::Html;
@@ -7,18 +8,44 @@ use axum::Json;
 use axum::Router;
 use local_ip_address::local_ip;
 use serde_json::json;
+use tower_http::cors::Any;
+use tower_http::cors::CorsLayer;
+use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
 #[tokio::main]
 async fn main() {
+    // initialize tracing
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG")
+                .unwrap_or_else(|_| "send_file_core=debug,tower_http=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // define cors scope
+    let cors_layer = CorsLayer::new()
+        .allow_headers(Any)
+        .allow_methods(Any)
+        .allow_origin(Any);
+    // define file limit layer as 10GB
+    let file_limit = DefaultBodyLimit::max(10 * 1024 * 1024 * 1024);
+
     let my_local_ip = local_ip().unwrap();
     let port: u16 = portpicker::pick_unused_port().expect("failed to get an unused port");
     let ip_address = format!("{:?}:{:?}", my_local_ip, port);
-    println!("server running on {:?}", ip_address);
+    println!("server running on http://{:?}", ip_address);
 
-    // build our application with a route
+    // build our application with the required routes
+    // the index route for debugging
+    // and the upload route for file upload
     let app = Router::new()
         .route("/", get(handler))
-        .route("/upload", post(recieve_files));
+        .route("/upload", post(recieve_files))
+        .layer(file_limit)
+        .layer(cors_layer)
+        .layer(tower_http::trace::TraceLayer::new_for_http());
 
     // run it
     axum::Server::bind(&ip_address.parse().unwrap())
@@ -30,10 +57,11 @@ async fn main() {
 async fn handler() -> Html<String> {
     Html(format!(
         "
+         <!doctype html>
    <html>
+<head>
 
-<head></head>
-
+</head>
 <body>
 <h1> hey man </h1>
     <form action='/upload' method='post' enctype='multipart/form-data'>
@@ -58,7 +86,7 @@ async fn recieve_files(mut multipart: Multipart) -> impl IntoResponse {
         let data = field.bytes().await.unwrap();
 
         println!(
-            "Length of `{}` (`{}`: `{}`) is {} bytes",
+            "Length of `{}` (`{}`: `{}` {}) is bytes",
             name,
             file_name,
             content_type,
--- sandbox/src/server_with_multipart.rs
+++ sandbox/src/server_with_multipart.rs
@@ -1,25 +1,24 @@
-use axum::{
-    response::{Html, IntoResponse},
-    routing::{get, post},
-    Router,
-};
-use axum::extract::Multipart
+use axum::extract::Multipart;
+use axum::http::StatusCode;
+use axum::response::Html;
+use axum::response::IntoResponse;
+use axum::routing::{get, post};
+use axum::Json;
+use axum::Router;
 use local_ip_address::local_ip;
+use serde_json::json;
 
 #[tokio::main]
 async fn main() {
     let my_local_ip = local_ip().unwrap();
     let port: u16 = portpicker::pick_unused_port().expect("failed to get an unused port");
     let ip_address = format!("{:?}:{:?}", my_local_ip, port);
-
-    println!("server running on {ip_address:?}");
-    let file_limit = RequestBodyLimitLayer::new(10 * 1024 * 1024 * 1024);
+    println!("server running on {:?}", ip_address);
 
     // build our application with a route
     let app = Router::new()
         .route("/", get(handler))
-        .route("/files", post(recieve_files))
-        .layer(file_limit);
+        .route("/upload", post(recieve_files));
 
     // run it
     axum::Server::bind(&ip_address.parse().unwrap())
@@ -28,25 +27,33 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html(
-        r#"
-        <h1> sample upload </h1>
-     <form action="http://localhost:3000/upload" method="post" enctype="multipart/form-data">
+async fn handler() -> Html<String> {
+    Html(format!(
+        "
+   <html>
+
+<head></head>
+
+<body>
+<h1> hey man </h1>
+    <form action='/upload' method='post' enctype='multipart/form-data'>
         <label>
             Upload file:
-            <input type="file" name="file" multiple>
+            <input type='file' name='file' multiple>
         </label>
-        <input type="submit" value="Upload files">
+        <input type='submit' value='Upload files'>
     </form>
-    "#,
-    )
+</body>
+
+</html>
+   "
+    ))
 }
 
 async fn recieve_files(mut multipart: Multipart) -> impl IntoResponse {
     while let Some(field) = multipart.next_field().await.unwrap() {
         let name = field.name().unwrap().to_string();
-        // let file_name = field.file_name().unwrap().to_string();
+        let file_name = field.file_name().unwrap().to_string();
         let content_type = field.content_type().unwrap().to_string();
         let data = field.bytes().await.unwrap();
 
@@ -58,4 +65,10 @@ async fn recieve_files(mut multipart: Multipart) -> impl IntoResponse {
             data.len()
         );
     }
+    (
+        StatusCode::OK,
+        Json(json!({
+            "Success":true
+        })),
+    )
 }
