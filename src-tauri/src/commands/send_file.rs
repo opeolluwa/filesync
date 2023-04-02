f3df86401d1b080c7e1104e5f76ba721bae26363
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-04-02 12:47:14 +0100

    implemented file server
    the core application server to recieve and save file is successfully implemented

--- src-tauri/src/commands/send_file.rs
+++ src-tauri/src/commands/send_file.rs
@@ -1,5 +1,6 @@
 use local_ip_address::local_ip;
-use reqwest::Response;
+// use reqwest::Response;
+use serde_json::{json, Value};
 use tokio::fs::File;
 use tokio::io::AsyncReadExt;
 
@@ -13,18 +14,21 @@ use super::CommandData;
 #[tauri::command]
 pub async fn share_file_with_peer(
     file_path: String,
-    server_id: u64,
-) -> Result<CommandData<Response>, CommandData<()>> {
+    server_id: u16,
+) -> Result<CommandData<Value>, CommandData<()>> {
     let mut file = File::open(file_path).await.unwrap();
     let mut vec = Vec::new();
-    file.read_to_end(&mut vec);
+    println!("file content {vec:?}");
+    let _ = file.read_to_end(&mut vec).await.unwrap();
     // file.read_to_end(&mut vec).await.unwrap();
     let client = reqwest::Client::new();
-
+    println!("yo! im in here");
     // get the IP address of the share network
     let my_local_ip = local_ip().unwrap();
-    let ip_address = format!("{:?}:{:?}", my_local_ip, server_id);
-    let res = client
+    let ip_address = format!("http://{:?}:{:?}/upload", my_local_ip, server_id);
+
+    println!("my client id is {ip_address}");
+    let _res = client
         .post(&ip_address)
         .header("content-type", "application/octet-stream")
         .body(vec)
@@ -32,6 +36,17 @@ pub async fn share_file_with_peer(
         .await
         .unwrap();
 
+    println!("the response here {_res:?}");
+
     // return an instance of the command data
-    Ok(CommandData::new("file successfully sent", true, res))
+    // Ok(CommandData::new("file successfully sent", true, res))
+    Ok(CommandData::new(
+        "file successfully sent",
+        true,
+        json!({
+            "success":true,
+            // data:r
+        }),
+    ))
+    // todo!()
 }
