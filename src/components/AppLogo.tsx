159cd29801767bb0a783091d30b877ec4d33f958
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-05-02 07:24:38 +0100

    refactor: renamed app logo to sendFile from breeze

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -1,7 +1,7 @@
 export default function AppLogo() {
     return (
         <div className="bg-transparent font-medium text-center  dark:text-shilo-500">
-            Breeze
+            SendFile
         </div>
     );
 }
