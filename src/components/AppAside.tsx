29728be0a5aab479ecb7383c4b79295256f86580
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-05-06 09:21:48 +0100

    refactor(ui): removed recieved fikes section

--- src/components/AppAside.tsx
+++ src/components/AppAside.tsx
@@ -51,23 +51,8 @@ const sampleReceivedFiles: FileInterface[] = [
 export default function Aside() {
     return (
         <aside className='col-span-4 pt-10 px-8  bg-[rgba(226,233,252,255)]   dark:border-l-mirage-x-700 dark:border-l dark:bg-mirage-600' >
-            <h2 className='font-medium dark:text-gray-400 flex items-center justify-between mb-10'>
-                Received Files
-
-            </h2>
-            {
-                /**
-                 * use state management to display files here
-                 * a procedure to determine the file type too and the right file icon should be aded
-                 */
-            }
-            {
-                sampleReceivedFiles.length == 0 ? <p className='text-gray-500 dark:text-gray-400'>No files received yet</p> : sampleReceivedFiles.map((file, index) => (
-                    <FileCard key={index} fileType={file.fileType} fileName={file.fileName} fileSize={file.fileSize} status={file.status} />
-                ))
-            }
-
-            <h2 className='font-medium mt-12 dark:text-gray-400 flex items-center justify-between mb-10'>
+          
+            <h2 className='font-bold mt-12 dark:text-gray-400 flex items-center justify-between mb-10'>
                 Sent Files
 
             </h2>
