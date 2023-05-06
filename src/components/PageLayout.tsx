f7f1a39fba7a17bc2cd74e4d6bdf65230a5af993
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-05-06 09:25:11 +0100

    refactor: made page title bolder

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,6 +1,7 @@
 import { goToPrevPage, goToNextPage } from "@/utils"
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline"
 import SearchBar from "./SearchBar";
+import PageTitle from "./PageTitle";
 
 interface Props {
     children: React.ReactNode,
@@ -14,11 +15,13 @@ export default function QuickAccessLayout({ children, pageTitle, includeSearchBa
     return (
         <>
             {/** page title  and navigation icons  */}
-            <h2 className="flex mb-10 font-medium dark:text-gray-300 justify-between items-center">
+            <div className="flex mb-10 font-medium dark:text-gray-300 justify-between items-center">
                 <ChevronLeftIcon className="text-gary-300 dark:text-gray-200 dark:hover:text-shilo-700 w-6 h-6 text-medium cursor-pointer mr-8" onClick={goToPrevPage} />
-                {pageTitle}
+                <h2 className='text-xl font-bold text-gray-700 dark:text-gray-300'>
+                    {pageTitle}
+                </h2>
                 <ChevronRightIcon className="text-gary-300 dark:text-gray-200 dark:hover:text-shilo-700 w-6 h-6 text-medium cursor-pointer mr-8" onClick={goToNextPage} />
-            </h2>
+            </div>
 
             {/** search bar  */}
             {includeSearchBar && <SearchBar onSearch={function (city: string): void {
