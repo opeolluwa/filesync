f93788b4bce60afffb705650315e90d6585814a1
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-05-17 11:12:26 +0100

    refactor: set search bar to hidden

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -15,7 +15,7 @@ export default function QuickAccessLayout({ children, pageTitle, includeSearchBa
     return (
         <>
             {/** page title  and navigation icons  */}
-            <div className="flex mb-10 font-medium dark:text-gray-300 justify-between items-center">
+            <div className="hidden mb-10 font-medium dark:text-gray-300 justify-between items-center">
                 <ChevronLeftIcon className="text-gary-300 dark:text-gray-200 dark:hover:text-shilo-700 w-6 h-6 text-medium cursor-pointer mr-8" onClick={goToPrevPage} />
                 <h2 className='text-xl font-bold text-gray-700 dark:text-gray-300'>
                     {pageTitle}
