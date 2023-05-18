7849294bd9e60ad69d78b2ed77eef50ba55cbad0
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-05-18 15:39:45 +0100

    refactor: removed dead code

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 interface Props {
     title: string
 }
@@ -7,10 +5,6 @@ export default function PageTitle({ title }: Props) {
     return (
         <h2 className='font-bold  dark:text-gray-400 flex items-center justify-between mb-10'>
             {title}
-
         </h2>
-      /*   <h2 className='mt-20 mb-8  font-bold text-gray-700 dark:text-gray-300'>
-            {title}
-        </h2> */
     )
 }
