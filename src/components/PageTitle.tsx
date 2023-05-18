e6643f096dee258a9bb088906d9bab611875b3b6
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-05-18 15:10:16 +0100

    refactor: bumped down font size

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 export default function PageTitle({ title }: Props) {
     return (
-        <h2 className='mt-20 mb-8  text-2xl font-bold text-gray-700 dark:text-gray-300'>
+        <h2 className='mt-20 mb-8  font-bold text-gray-700 dark:text-gray-300'>
             {title}
         </h2>
     )
