34888a92bddf7e782560d69192a77ca789353a20
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-05-18 15:43:14 +0100

    refactor: added connection tab

--- src/components/AppNavigation.tsx
+++ src/components/AppNavigation.tsx
@@ -1,10 +1,9 @@
 // import Home from '@/pages/home'
-import { Cog8ToothIcon, HomeIcon, FolderOpenIcon, InformationCircleIcon, ShareIcon, ClockIcon } from '@heroicons/react/24/outline';
-import { Cog8ToothIcon as SolidCog8ToothIcon, HomeIcon as SolidHomeIcon, FolderOpenIcon as SolidFolderIconOpen, InformationCircleIcon as SolidInformationIcon, ShareIcon as SolidShareIcon, ClockIcon as SolidClockIcon } from '@heroicons/react/24/solid'
+import { Cog8ToothIcon, HomeIcon, ArrowsRightLeftIcon, FolderOpenIcon, InformationCircleIcon, ShareIcon, ClockIcon, SignalIcon } from '@heroicons/react/24/outline';
+import { Cog8ToothIcon as SolidCog8ToothIcon, HomeIcon as SolidHomeIcon, FolderOpenIcon as SolidFolderIconOpen, InformationCircleIcon as SolidInformationIcon, ShareIcon as SolidShareIcon, SignalIcon as SolidSignalIcon, ClockIcon as SolidClockIcon } from '@heroicons/react/24/solid'
 import { DialogFilter, message, ask } from '@tauri-apps/api/dialog';
 import { open } from '@tauri-apps/api/dialog';
 import { invoke } from '@tauri-apps/api/tauri';
-import HostSpotIcon from './icons/HostSpotIcon';
 import { goToPage as gotoPage } from '@/utils';
 import { Fragment, useEffect, useState } from 'react'
 
@@ -54,12 +53,11 @@ const allowedExtension: DialogFilter[] = [{ name: 'image', extensions: ['ai', 'd
 { name: 'document', extensions: ['txt', 'rtf', 'c', 'h', 'cpp', 'hpp', 'cxx', 'hxx', 'java', 'js', 'rb', 'py', 'cs', 'm', 'sh', 'php', 'css', 'go'] }]
 
 interface Route {
-    path: string, // the path string
     icon: any, // the route icon
     name: string, // the route name
     alternateIcon: any, // the icon to show on hover or active state
     action?: () => any // action that will be executed when the route is clicked
-
+    path?: string, // the path string
 }
 // the port on which th application urn for the sender PC 
 interface SenderProps {
@@ -81,9 +79,7 @@ interface SystemInformation {
 
 
 export default function AppNavigation() {
-    let [isModalOpen, setModalState] = useState(false)
     let [systemInformation, setSystemInformation] = useState({} as SystemInformation);
-
 
     useEffect(() => {
         invoke('get_system_information').then((sysInfo) => {
@@ -100,17 +96,16 @@ export default function AppNavigation() {
 
     },
     {
-        path: '/share',
-        icon: <ShareIcon className='w-6 h-6' />,
-        name: 'Shared with me',
-        alternateIcon: <SolidShareIcon className='w-6 h-6' />,
+        icon: <SignalIcon className='w-6 h-6' />,
+        name: 'Connection',
+        alternateIcon: <SolidSignalIcon className='w-6 h-6' />,
         action: () => gotoPage({ routePath: "settings" })
 
     },
     {
         path: '/share',
         icon: <ClockIcon className='w-6 h-6' />,
-        name: 'recent files',
+        name: 'History',
         alternateIcon: <SolidClockIcon className='w-6 h-6' />,
         action: () => gotoPage({ routePath: "settings" })
 
@@ -123,6 +118,14 @@ export default function AppNavigation() {
         name: 'File Manager'
     },
 
+    {
+        path: '/share',
+        icon: <ShareIcon className='w-6 h-6' />,
+        name: 'Shared files',
+        alternateIcon: <SolidShareIcon className='w-6 h-6' />,
+        action: () => gotoPage({ routePath: "settings" })
+
+    },
     {
         path: '/settings',
         icon: <Cog8ToothIcon className='w-6 h-6' />,
@@ -148,23 +151,23 @@ export default function AppNavigation() {
                     overflowY: "hidden"
                 }
             }>
-
                 <ul className=' h-full flex flex-col px-4 pl-6'>
                     {
                         /**
-                         * show the icon and the name side by side
+                         * show the icon and the name side by side on full screen mode 
+                         * otherwise, hide the name and show the icons only 
                          */
                     }
                     {routes.map((route, index) => (
-                        <li key={index} className='flex h-6 my-8 first:mt-10 last:mb-20 text-app-500 cursor-pointer'>
-                            <span onClick={route.action} className='cursor-pointer'>
+                        <li key={index} className='flex h-6 my-8 lg:my-8 first:mt-10 last:mb-20 text-app-500 cursor-pointer'>
+                            <span onClick={route.action /**action to perform on route clicked */} className='cursor-pointer'>
                                 <span className='sr-only'>
                                     {route.path}
                                 </span>
                                 <div className='gap-2 justify-center align-center flex capitalize'>
-                                    {route.icon}
+                                    {route.icon /**the route icon */}
                                     <span className='hidden lg:block' id='route__name'>
-                                        {route.name}
+                                        {route.name /** the route name */}
                                     </span>
                                 </div>
                             </span>
