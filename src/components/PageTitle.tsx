75ddd323e65a13e7ba1a57d94731e9494e05853a
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-05-18 15:27:20 +0100

    refactor: updated font size

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -5,8 +5,12 @@ interface Props {
 }
 export default function PageTitle({ title }: Props) {
     return (
-        <h2 className='mt-20 mb-8  font-bold text-gray-700 dark:text-gray-300'>
+        <h2 className='font-bold  dark:text-gray-400 flex items-center justify-between mb-10'>
             {title}
+
         </h2>
+      /*   <h2 className='mt-20 mb-8  font-bold text-gray-700 dark:text-gray-300'>
+            {title}
+        </h2> */
     )
 }
