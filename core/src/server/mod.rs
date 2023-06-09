f5806bc43ac9db35aa352d9cfd38aeefff5cfcf4
Muhammad Saboor <muhammad.saboor@trapmine.com>
2023-06-09 11:48:51 +0300

    style: remove redundant ok

--- core/src/server/mod.rs
+++ core/src/server/mod.rs
@@ -68,8 +68,7 @@ pub async fn core_server() {
 
     // the core server
     let my_local_ip = utils::ip_manager::autodetect_ip_address()
-        .ok()
-        .expect("Invalid Ip address detected")
+        .expect("No Ip address detected")
         .parse::<Ipv4Addr>()
         .unwrap();
     let ip_address = format!("{:?}:{:?}", my_local_ip, *SERVER_PORT as u64);
