8629867bba928e91517a9655052be689476b89a2
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-06-14 09:05:15 +0100

    refactor(ui component): create a file-icon component

--- src/components/FileCard.tsx
+++ src/components/FileCard.tsx
@@ -38,24 +38,16 @@ export default function FileCard({
   fileType,
   status,
 }: FileInterface) {
-  const fileT = fileType as any;
   return (
     <>
       <div className="flex justify-between items-center my-8 flex-wrap bg-[#edeffb]  border-gray-900  p-3 rounded-lg shadow-md shadow-gray-300 cursor-pointer dark:shadow-none hover:shadow-sm hover:shadow-gray-400 transition-shadow ease-in-out">
-        <Image
-          src={`/images/mime/${fileType}.png`} // Route of the image file
-          height={120} // Desired size with correct aspect ratio
-          width={120} // Desired size with correct aspect ratio
-          alt="file card icon"
-          className="w-[48px] col-span-1" // automatic height calculation
-        />
-
+        <FileIcon fileType={fileType} />
         <div className="flex flex-col text-ellipsis">
           <h5 className="font-medium text-gray-500 overflow-clip text-ellipsis">
             {fileName}
           </h5>
           <div
-            className="flex gap-3 mt[1.5px] text-gray-400  italic text-xs height={30} // Desired size with correct aspect ratio
+            className="flex gap-3 mt[1.5px] text-gray-400  italic text-xs height={30} 
                 width={30} "
           >
             <span>{computeFileSize(fileSize)}</span>{" "}
@@ -77,6 +69,167 @@ export default function FileCard({
           )}
         </div>
       </div>
+    </>
+  );
+}
+
+function FileIcon({ fileType }: { fileType: string }) {
+  const fileExtension = fileType.trim().toLowerCase();
+  const images = [
+    "ai",
+    "dxf",
+    "odg",
+    "fodg",
+    "svg",
+    "svgz",
+    "bmp",
+    "gif",
+    "ico",
+    "jpg",
+    "jpeg",
+    "png",
+    "psd",
+    "pdd",
+    "tga",
+    "tiff",
+    "xcf",
+    "xpm",
+  ];
+  const audio = [
+    "au",
+    "aif",
+    "aifc",
+    "aiff",
+    "wav",
+    "flac",
+    "la",
+    "pac",
+    "m4a",
+    "ape",
+    "wv",
+    "wma",
+    "ast",
+    "mp2",
+    "mp3",
+    "spx",
+    "aac",
+    "mpc",
+    "ra",
+    "ogg",
+    "mid",
+    "m3u",
+    "pls",
+  ];
+  const powerpoint = [
+    "ppt",
+    "pot",
+    "pps",
+    "pptx",
+    "pptm",
+    "potx",
+    "potm",
+    "ppam",
+    "ppsx",
+    "ppsm",
+    "sldx",
+    "sldm",
+    "odp",
+    "fodp",
+    "otp",
+  ];
+  const video = [
+    "webm",
+    "mkv",
+    "flv",
+    "vob",
+    "ogv",
+    "drc",
+    "avi",
+    "mov",
+    "qt",
+    "wmv",
+    "rm",
+    "rmvb",
+    "asf",
+    "mp4",
+    "m4p",
+    "m4v",
+    "mpg",
+    "mpeg",
+    "mpe",
+    "mpv",
+    "3gp",
+    "3g2",
+    "mxf",
+    "aff",
+    "m2ts",
+    "mts",
+  ];
+  const excel = [
+    "xls",
+    "xlt",
+    "xlm",
+    "xlsx",
+    "xlsm",
+    "xltx",
+    "xltm",
+    "xla",
+    "xlam",
+    "ods",
+    "fods",
+    "ots",
+  ];
+  return (
+    <>
+      {images.includes(fileExtension) ? (
+        <Image
+          src={`/images/mime/${fileExtension}.png`}
+          height={120}
+          width={120}
+          alt="file card icon"
+          className="w-[48px] col-span-1"
+        />
+      ) : audio.includes(fileExtension) ? (
+        <Image
+          src={`/images/mime/${fileExtension}.png`}
+          height={120}
+          width={120}
+          alt="file card icon"
+          className="w-[48px] col-span-1"
+        />
+      ) : video.includes(fileExtension) ? (
+        <Image
+          src={`/images/mime/${fileExtension}.png`}
+          height={120}
+          width={120}
+          alt="file card icon"
+          className="w-[48px] col-span-1"
+        />
+      ) : excel.includes(fileExtension) ? (
+        <Image
+          src={`/images/mime/${fileExtension}.png`}
+          height={120}
+          width={120}
+          alt="file card icon"
+          className="w-[48px] col-span-1"
+        />
+      ) : powerpoint.includes(fileExtension) ? (
+        <Image
+          src={`/images/mime/${fileExtension}.png`}
+          height={120}
+          width={120}
+          alt="file card icon"
+          className="w-[48px] col-span-1"
+        />
+      ) : (
+        <Image
+          src={`/images/mime/file.png`}
+          height={120}
+          width={120}
+          alt="file card icon"
+          className="w-[48px] col-span-1"
+        />
+      )}
     </>
   );
 }
