39baf666808b01c7845ef31b03727a81b95551e3
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-06-17 18:28:08 +0100

    updates(ui): updated the share files page

--- src/pages/share.tsx
+++ src/pages/share.tsx
@@ -1,20 +1,29 @@
 import PageLayout from "@/components/PageLayout";
-import { SetStateAction, useState } from "react";
+import { SetStateAction, useRef, useState } from "react";
 
 /**
  * @function helpPage -  A page responsible for guiding users on various actions
  * @returns tsx
  */
 export default function ShareFiles() {
-  const [file, setFile] = useState(null);
-  const handleChange = (file: SetStateAction<null>) => {
-    setFile(file);
+  const ref = useRef<HTMLInputElement>(null);
+  // 1. add state for tracking the selected files
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+
+  // 2. pass the click event to the hidden input element to trigger the file selection.
+  const handleClick = () => {
+    ref.current?.click();
   };
 
+  // 3. convert FileList to File[]
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = Array.from(e.currentTarget.files ?? []);
+    setSelectedFiles(files);
+  };
   return (
     <>
       <PageLayout pageTitle={"Shared files"} includeSearchBar={false}>
-        <div className=" p-8">
+        <div className=" p-8" >
           <label className="flex justify-center w-full h-[500px] px-4 transition bg-transparent border-[2px] border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
             <span className="flex items-center space-x-2">
               <svg
@@ -36,11 +45,33 @@ export default function ShareFiles() {
                 <span className="text-app-600 underline"> browse</span>
               </span>
             </span>
-            <form action="" encType="multipart">
-              <input type="file" name="file_upload" className="hidden" />
-            </form>
+            {/* <form onSubmit={handleUpload} encType="multipart"> */}
+            <div onClick={handleClick}>
+              <input
+                type="file"
+                name="file_upload"
+                className="hidden"
+                multiple={true}
+                onChange={handleChange}
+              />
+            </div>
+            {/* </form> */}
           </label>
         </div>
+
+        {/* 6. display selected files */}
+        {!!selectedFiles.length && (
+          <div className="p-4 mt-4 bg-violet-50 overflow-hidden text-ellipsis">
+            <p>Selected Files:</p>
+            {selectedFiles.map((file, i) => {
+              return (
+                <span key={i} className="text-violet-500 whitespace-nowrap">
+                  {file.name}
+                </span>
+              );
+            })}
+          </div>
+        )}
       </PageLayout>
     </>
   );
