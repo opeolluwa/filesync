b6b1a2d729e9933e1eb36996e5257ba8c4842ed2
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-06-20 21:59:39 +0100

    refactor(ui): updated the Upload components

--- src/pages/share.tsx
+++ src/pages/share.tsx
@@ -1,55 +1,51 @@
-import PageLayout from '@/components/PageLayout';
-import { SetStateAction, useContext, useRef, useState } from 'react';
-import { InboxOutlined } from '@ant-design/icons';
-import type { UploadProps } from 'antd';
-import { message, Upload } from 'antd';
-import Dragger from 'antd/es/upload/Dragger';
-import { FileContext } from '@/store/context';
+import PageLayout from "@/components/PageLayout";
+import {  useContext,  } from "react";
+import type { UploadProps } from "antd";
+import { message, Upload } from "antd";
+import { FileContext, FileTransferStatus } from "@/store/context";
+import { CloudArrowUpIcon } from "@heroicons/react/24/outline";
+
 
 /**
- * @function helpPage -  A page responsible for guiding users on various actions
+ * @function sharePage -  A page responsible for guiding users on various actions
  * @returns tsx
  */
 export default function ShareFiles() {
-	const { Dragger } = Upload;
-	const { onUpdate } = useContext(FileContext);
+  const { Dragger } = Upload;
+  const { onUpdate } = useContext(FileContext);
 
-	const props: UploadProps = {
-		name: 'file',
-		multiple: true,
-		action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-		onChange(info) {
-			const { status } = info.file;
-			if (status !== 'uploading') {
-				onUpdate(info.fileList);
-				console.log(info.file, info.fileList);
-			}
-			if (status === 'done') {
-				message.success(`${info.file.name} file uploaded successfully.`);
-			} else if (status === 'error') {
-				message.error(`${info.file.name} file upload failed.`);
-			}
-		},
-		onDrop(e) {
-			console.log('Dropped files', e.dataTransfer.files);
-		},
-	};
-	return (
-		<>
-			<PageLayout pageTitle={'Shared files'} includeSearchBar={false}>
-				<Dragger className='h-[500px] block' {...props}>
-					<p className='ant-upload-drag-icon'>
-						<InboxOutlined rev={undefined} />
-					</p>
-					<p className='ant-upload-text'>
-						Click or drag file to this area to upload
-					</p>
-					<p className='ant-upload-hint'>
-						Support for a single or bulk upload. Strictly prohibited from
-						uploading company data or other banned files.
-					</p>
-				</Dragger>
-			</PageLayout>
-		</>
-	);
+  const props: UploadProps = {
+    name: "file",
+    multiple: true,
+	//TODO: use actual API fetch from the peer device information
+    action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== FileTransferStatus.UPLOADING) {
+        onUpdate(info.fileList);
+        console.log(info.file, info.fileList);
+        // TODO: added uploaded files to application transfer history
+      }
+      if (status === FileTransferStatus.COMPLETED) {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === FileTransferStatus.ERROR) {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+    onDrop(e) {
+      console.log("Dropped files", e.dataTransfer.files);
+    },
+  };
+  return (
+    <>
+      <PageLayout pageTitle={"Shared files"} includeSearchBar={false}>
+        <Dragger className="h-[500px] block" {...props}>
+          <CloudArrowUpIcon className="text-app-300  text-center small w-20 mx-auto" />
+          <p className="text-gray-400 leading-2">
+            Drop files here to share or click to browse
+          </p>
+        </Dragger>
+      </PageLayout>
+    </>
+  );
 }
