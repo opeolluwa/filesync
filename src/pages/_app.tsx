dd1d439670b47aa0b9fba47bc179b804ef19538b
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-06-21 06:20:54 +0100

    added system information store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import Layout from '@/components/AppLayout';
-import FileStore from '@/store/context';
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
+import Layout from "@/components/AppLayout";
+import FileStore from "@/store/context";
+import SystemInfoStore from "@/store/sys-info";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<FileStore>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</FileStore>
-	);
+  return (
+    <SystemInfoStore>
+      <FileStore>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </FileStore>
+    </SystemInfoStore>
+  );
 }
