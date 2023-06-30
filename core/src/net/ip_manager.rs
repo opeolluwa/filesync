56e24a4ad93604aec2da206af56053e2ba2cea61
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-06-30 05:59:23 +0100

    refactor: tried fixing Windows OS cI

--- core/src/net/ip_manager.rs
+++ core/src/net/ip_manager.rs
@@ -2,7 +2,7 @@ use local_ip_address::local_ip;
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
+#[cfg(target_family = "unix")]
 extern crate pnet_datalink;
 extern crate std;
 
