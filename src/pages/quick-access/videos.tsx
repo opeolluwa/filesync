95c0e21b337ef327bf51424a10a050c4ff2b150d
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-07-03 18:32:22 +0100

    chore: updated file name to WiShare

--- src/pages/quick-access/videos.tsx
+++ src/pages/quick-access/videos.tsx
@@ -1,3 +1,4 @@
+import AudioList from "@/components/AudioList";
 import PageLayout from "@/components/layout/PageLayout";
 
 export default function Desktop() {
@@ -6,7 +7,7 @@ export default function Desktop() {
       <PageLayout pageTitle={"Videos"} includeSearchBar={true}>
         <div>
           <h1>Help</h1>
-
+          <AudioList />
           <div>some help content goes here</div>
         </div>
       </PageLayout>
