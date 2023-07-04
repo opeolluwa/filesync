42cdb60e875d8c921d319f3eadb5be417a3791fb
zoo868e <zoo868e@gmail.com>
2023-07-05 01:15:16 +0800

    Add functions for Windows hotspot management
    This commit adds functions for creating and managing a hotspot on
    Windows. The `create_ap` function checks if the device supports the
    hotspot feature and sets up the hotspot name and password. It then
    creates a new hotspot and starts it. The `turn_off_hotspot` function
    stops the hotspot.
    
    The `is_support_hotspot` function uses the `netsh` command to check if
    the device supports the hosted network. The `create_hotspot` function
    uses the `netsh` command to create a new hotspot with the provided SSID
    and key. The `start_hotspot` function starts the created hotspot, and
    the `stop_hotspot` function stops the running hotspot.
    
    These functions provide basic functionality for managing a hotspot on
    Windows.
    
    Please review and test the code.

--- core/src/net/windows_hotspot.rs
+++ core/src/net/windows_hotspot.rs
@@ -1,28 +1,129 @@
 use std::process::Command;
+fn command_output_to_string(output: &Vec<u8>) -> String {
+    let _output = std::str::from_utf8(&output);
+    match _output {
+        Ok(s) => s.to_string(),
+        Err(_) => "None".to_string(),
+    }
+}
+/// Create hotspot on windows
 #[allow(dead_code)]
 pub fn create_ap() {
+    // Check if the device support the hotspot feature
+    match is_support_hotspot() {
+        Ok(false) => {
+            println!("Hotspot is not supported");
+            return;
+        }
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+        Ok(true) => println!("Hotspot is supported"),
+    };
     // Set up the hotspot name and password
     let hotspot_name = "MyHotspot";
     let hotspot_password = "MyPassword123";
 
-    // Execute the netsh commands to create the hotspot
-    let output = Command::new("netsh")
+    // Create a new hotspot
+    match create_hotspot(hotspot_name, hotspot_password) {
+        Ok(_) => println!("Created a new hotspot"),
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
+
+    // Start the hotspot
+    match start_hotspot() {
+        Ok(_) => println!("Started a new hotspot"),
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
+}
+
+#[warn(dead_code)]
+pub fn turn_off_hotspot() {
+    match stop_hotspot() {
+        Ok(_) => println!("Stop the hotspot successfully"),
+        Err(e) => println!("{}", e),
+    }
+}
+
+fn is_support_hotspot() -> Result<bool, String> {
+    match Command::new("netsh")
+        .args(["wlan", "show", "drivers"])
+        .output()
+    {
+        Ok(output) => {
+            if command_output_to_string(&output.stdout)
+                .as_str()
+                .contains("Hosted network supported  : Yes")
+            {
+                Ok(true)
+            } else {
+                Ok(false)
+            }
+        }
+        Err(_) => Err(format!("Failed to get the wlan drivers information")),
+    }
+}
+
+fn create_hotspot(ssid: &str, key: &str) -> Result<(), String> {
+    match Command::new("netsh")
         .args([
             "wlan",
             "set",
             "hostednetwork",
-            "mode=allow",
-            &("ssid=".to_owned() + hotspot_name),
-            &("key=".to_owned() + hotspot_password),
+            &("ssid=".to_owned() + ssid),
+            &("key=".to_owned() + key),
         ])
         .output()
-        .expect("Failed to execute 'netsh' command.");
+    {
+        Ok(output) => {
+            if let true = output.status.success() {
+                return Ok(());
+            } else {
+                return Err(format!(
+                    "Failed to create hotspot: {}",
+                    command_output_to_string(&output.stderr)
+                ));
+            }
+        }
+        Err(_) => return Err(format!("Failed to execute create hotspot through netsh.")),
+    }
+}
 
-    // Check the command execution result
-    if output.status.success() {
-        println!("Hotspot created successfully.");
-    } else {
-        let error_message = String::from_utf8_lossy(&output.stderr);
-        println!("Failed to create hotspot: {}", error_message);
+fn start_hotspot() -> Result<(), String> {
+    match Command::new("netsh")
+        .args(["wlan", "start", "hostednetwork"])
+        .output()
+    {
+        Ok(output) => {
+            if command_output_to_string(&output.stdout).contains("The hosted network started.") {
+                return Ok(());
+            } else {
+                return Err(format!("Failed to start the hosted network"));
+            }
+        }
+        Err(_) => return Err(format!("Failed to start the hosted network through netsh")),
+    }
+}
+
+fn stop_hotspot() -> Result<(), String> {
+    match Command::new("netsh")
+        .args(["wlan", "stop", "hostednetwork"])
+        .output()
+    {
+        Ok(output) => {
+            if command_output_to_string(&output.stdout).contains("The hosted network stoped.") {
+                return Ok(());
+            } else {
+                return Err(format!("Failed to stop the hosted network"));
+            }
+        }
+        Err(_) => return Err(format!("Failed to stop the hosted network through netsh")),
     }
 }
