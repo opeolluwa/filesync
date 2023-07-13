945244d0b966389aebc1b82737aac8576aecd137
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-07-13 10:17:02 +0100

    refactor: updated import path

--- core/src/main.rs
+++ core/src/main.rs
@@ -19,30 +19,25 @@ lazy_static! {
 }
 
 fn main() -> Result<(), tauri::Error> {
-    // create  ap
-    /*    let config = net::linux_hotspot::create_hotspot();
-       println!("{:#?}", config);
-    */
     // run core the server in a separate thread from tauri
     tauri::async_runtime::spawn(server::core_server());
     tauri::Builder::default()
         .plugin(tauri_plugin_upload::init())
         .plugin(tauri_plugin_sqlite::init())
         .invoke_handler(tauri::generate_handler![
-            command::utils::get_ip_address,
-            command::audio::fetch_audio_files,
-            command::video::fetch_video_files,
-            command::image::fetch_images,
-            command::video::fetch_video_files,
-            command::utils::close_splashscreen,
-            command::send_file::share_file_with_peer,
-            command::utils::get_system_information,
-            command::documents::fetch_documents,
-            command::search::search_home_dir,
-            command::hotspot::create_wifi_hotspot,
-            command::hotspot::kill_wifi_hotspot,
-            command::connect_with_qr_code::generate_qr_code
+            get_ip_address,
+            fetch_audio_files,
+            fetch_video_files,
+            fetch_images,
+            fetch_video_files,
+            close_splashscreen,
+            share_file_with_peer,
+            get_system_information,
+            fetch_documents,
+            search_home_dir,
+            create_wifi_hotspot,
+            kill_wifi_hotspot,
+            generate_qr_code
         ])
         .run(tauri::generate_context!())
-    // .expect("error while running tauri application");
 }
