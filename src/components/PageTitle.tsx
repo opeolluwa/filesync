2d80d9ffc3cba21060b9e9469241e0fb08753aa6
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-07-13 09:53:22 +0100

    refactor

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,10 +1,17 @@
 interface Props {
-    title: string
+  title: string;
+  styles?: string;
 }
-export default function PageTitle({ title }: Props) {
-    return (
-        <h2 className='font-bold  dark:text-gray-400 flex items-center justify-between mb-12'>
-            {title}
-        </h2>
-    )
+export default function PageTitle({ title, styles }: Props) {
+  return (
+    <h2
+      className={
+        "font-bold  dark:text-gray-400 flex items-center justify-between" +
+        " " +
+        styles
+      }
+    >
+      {title}
+    </h2>
+  );
 }
