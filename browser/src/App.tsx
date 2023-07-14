fe2f6aee3440f801afeeae1c7c6597b5a8876a4c
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-07-14 11:05:05 +0100

    feat: implemented the mobile interface

--- browser/src/App.tsx
+++ browser/src/App.tsx
@@ -1,11 +1,105 @@
+import {
+  CameraIcon,
+  DocumentTextIcon,
+  FolderOpenIcon,
+  MusicalNoteIcon,
+  PlayIcon,
+} from "@heroicons/react/24/outline";
+import { MemoryInformation } from "./components/MemoryInformation.tsx";
+import PageTitle from "./components/PageTitle.tsx";
+import SearchBar from "./components/SearchBar.tsx";
 
-function App() {
-
-  return (
-    <>
-     
-    </>
-  )
+interface Tab {
+  name: string;
+  path: string;
+  icon: any;
+  color: string;
+  files?: number; // 200 Files
+  size?: string; // 56 MB
 }
 
-export default App
+function App() {
+  const routes: Array<Tab> = [
+    {
+      name: "document",
+      path: "/document",
+      color: "bg-yellow-50",
+      icon: <DocumentTextIcon className="text-yellow-500 w-6 h-6" />,
+    },
+    {
+      name: "images",
+      path: "/images",
+      color: "bg-sky-50",
+      icon: <CameraIcon className="text-sky-500 w-6 h-6" />,
+    },
+    {
+      name: "videos",
+      path: "/videos",
+      icon: <PlayIcon className="text-green-500 w-6 h-6" />,
+      color: "bg-green-50",
+    },
+    {
+      name: "audios",
+      path: "/audios",
+      color: "bg-red-50",
+      icon: <MusicalNoteIcon className="text-red-500 w-6 h-6" />,
+    },
+    {
+      name: "folders",
+      path: "/folder",
+      color: "bg-purple-50",
+      icon: <FolderOpenIcon className="text-purple-500 w-6 h-6" />,
+    },
+  ];
+  return (
+    <>
+      <div className="bg-app-700  text-white rounded-b-[24px] px-8 py-8">
+        <PageTitle styles="mb-4 pt-8" title={"Overview"} />
+        <SearchBar
+          onSearch={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+          placeholder={"search files"}
+        />
+      </div>
+
+      <div className="px-4 my-8">
+        {/* <p className="leading-3 capitalize text-gray-400">Space used </p> */}
+        <MemoryInformation systemName={"Sillicone"} freeMemory={"100 GB"} />
+      </div>
+
+      <div className="px-8 my-4">
+        {routes.map((route, index) => {
+          return (
+            <div
+              key={index}
+              className="flex items-center justify-between py-4 cursor-pointer hover:bg-gray-50"
+            >
+              <div className="flex items-center">
+                <div
+                  className={
+                    "w-10 h-10 rounded flex items-center justify-center mr-4 bg-red-50 " +
+                    route.color
+                  }
+                >
+                  {route.icon}
+                </div>
+                <div className="flex flex-col">
+                  <p className="text-sm capitalize">{route.name}</p>
+                  <p className="text-xs text-gray-400">
+                    {route.files ? route.files : "0"} files
+                  </p>
+                </div>
+              </div>
+              <div className="text-sm text-gray-400">
+                {route.size ? route.size : "0"} MB
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
