b9ce5946393e71fe3ea5efe9a808e4bb957742e5
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-07-14 01:45:23 +0100

    chore: updated connection components

--- src/pages/connection/send.tsx
+++ src/pages/connection/send.tsx
@@ -2,47 +2,76 @@ import PageLayout from "@/components/layout/PageLayout";
 import React from "react";
 import { Steps } from "antd";
 
-const description = "This is a description.";
-const steps = [
-  {
-    title: "First",
-    content: "First-content",
-  },
-  {
-    title: "Second",
-    content: "Second-content",
-  },
-  {
-    title: "Last",
-    content: "Last-content",
-  },
-];
-
 export default function HelpPage() {
   return (
     <>
       <PageLayout pageTitle={"Connect Device"} includeSearchBar={false}>
-        <>
-          <Steps
-            direction="vertical"
-            current={1}
-            items={[
-              {
-                title: "Finished",
-                description,
-              },
-              {
-                title: "In Progress",
-                description,
-              },
-              {
-                title: "Waiting",
-                description,
-              },
-            ]}
-          />
-        </>
+        <div>
+          <button
+            type="button"
+            className="flex justify-center rounded-md   px-4 py-2 text-sm font-medium border border-mirage-500 bg-gray-600 mt-2 text-white my-4"
+          >
+            Step 01:
+          </button>
+          <StepOne />
+          <button
+            type="button"
+            className="flex justify-center rounded-md   px-4 py-2 text-sm font-medium border border-mirage-500 bg-gray-600 mt-2 text-white my-4"
+          >
+            Step 02:
+          </button>
+          <StepTwo />
+
+          <button
+            type="button"
+            className="flex justify-center rounded-md   px-4 py-2 text-sm font-medium border border-mirage-500 bg-gray-600 mt-2 text-white my-4"
+          >
+            Step 03:
+          </button>
+          <StepTree />
+        </div>
       </PageLayout>
     </>
+  );
+}
+
+function StepOne() {
+  // tell use to create hotspot and add a button to do so
+  return (
+    <div className="my-8 fle flex-col pl-12 gap-2">
+      <p className="leading-3 hidden">
+        Create a WiFi hotspot on your device and connect to it from the other
+        device
+      </p>
+      <button
+        type="button"
+        className="flex justify-center rounded-md   px-4 py-2 text-sm font-medium border border-mirage-500 bg-app mt-2 text-white hidden"
+      >
+        Create Hotspot
+      </button>
+    </div>
+  );
+}
+
+function StepTwo() {
+  // tell user to connect peer device via wifi
+  return (
+    <div className="my-8 pl-12">
+      <p className="leading-3 text-gray-400 hidden">
+        Connect to the hotspot you created on the other device
+      </p>
+    </div>
+  );
+}
+
+function StepTree() {
+  // tell user to open up connection in app
+  return (
+    <div className="my-8 pl-12">
+      <div className="my-4 pl-4 text-gray hidden">
+        Open up the application or web browser on the other device and go to
+        this page
+      </div>
+    </div>
   );
 }
