0faa33a2db1ce6d2efa808e015370a19cb4b7dad
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-09-22 20:18:34 +0100

    chore: implemented connection page

---
+++ src/components/app/Text.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+interface Props {
+  context: string;
+  withStyle?: string;
+}
+export default function Text({ context, withStyle }: Props) {
+  return (
+    <p className={"leading-5 text-gray-500 dark:text-dark-500 " + withStyle}>
+      {context}
+    </p>
+  );
+}
