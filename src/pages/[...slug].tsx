1edea3835e729fab75809875b2a3483f685f446c
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-04-08 15:07:26 +0100

    feat: add folder renderer component

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -3,10 +3,12 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
-import { FileInterface } from "@/components/thumbnail";
+import FileCard, { FileInterface } from "@/components/thumbnail";
 import { AppData } from "@/types";
 import { invoke } from "@tauri-apps/api";
 import LoaderCircle from "@/components/loaders/LoaderCircle";
+import QuickAccessLayout from "@/components/layout/PageLayout";
+import { shareFile } from "@/utils";
 
 export default function PreviewMediaPage() {
   const router = useRouter();
@@ -15,40 +17,74 @@ export default function PreviewMediaPage() {
   const isFolder = searchParams.get("isFolder");
   const fileType = searchParams.get("fileType");
 
- const [data, setData] = useState(null);
- const [isLoading, setLoading] = useState(false);
+  // if it is a folder, get the files nd list them
+  if (isFolder) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [data, setData] = useState(null);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [isLoading, setLoading] = useState(false);
 
- // get the data from the application core
- useEffect(() => {
-   setLoading(true);
-   invoke("read_dir", { path: "documents" }).then((res) => {
-     setData(res as any);
-     setLoading(false);
-   });
- }, []);
+    // get the data from the application core
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+      setLoading(true);
+      invoke("read_dir", { path: filePath?.trim() }).then((res) => {
+        setData(res as any);
+        setLoading(false);
+      });
+    }, []);
 
- // typecast the response into AppData type
- const fetchedDocuments = data as unknown as AppData<Array<FileInterface>>;
- if (isLoading) {
-   return (
-     <>
-       <LoaderCircle />
-       <h2 className="font-xl font-bold mt-8">Loading...</h2>
-       <p className="leading-5 text-gray-400">
-         Please wait while we load your documents. This might take a while.
-       </p>
-     </>
-   );
- }
+    // typecast the response into AppData type
+     const fetchedFiles = data as unknown as AppData<Array<FileInterface>>;
+     if (isLoading) {
+       return (
+         <>
+           <LoaderCircle />
+           <h2 className="font-xl font-bold mt-8">Loading...</h2>
+           <p className="leading-5 text-gray-400">
+             Please wait while we load your documents. This might take a while.
+           </p>
+         </>
+       );
+     }
 
-  
+
+     // render them 
+      return (
+        <QuickAccessLayout
+          pageTitle={"Document"}
+          includeSearchBar={true}
+          searchBarText="search document"
+        >
+          <div>
+            <div className="flex flex-wrap  flex-grow gap-4 justify-start mt-12">
+              {fetchedFiles?.data.map((file, index) => (
+                <FileCard
+                  key={index}
+                  fileName={file.fileName}
+                  fileSize={file.fileSize}
+                  fileFormat={file.fileFormat}
+                  filePath={file.filePath}
+                  action={() => shareFile(file.filePath)}
+                  isHidden={file.isHidden}
+                  isFolder={file.isFolder}
+                />
+              ))}
+            </div>
+          </div>
+        </QuickAccessLayout>
+      );
+  }
+
+
+  if (isFolder) {
+    return;
+  }
   return (
     <div>
       <h1> file path: {filePath} </h1>
       <h1>file type {fileType}</h1>
-      <h1>
-       is folder {isFolder}
-      </h1>
+      <h1>is folder {isFolder}</h1>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat iste
       sequi aliquam aliquid! Iusto doloribus veritatis facere aliquam mollitia
       consequuntur hic, nostrum, fugiat pariatur incidunt, modi voluptatum illo
