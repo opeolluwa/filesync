47cd4be0c056b65be0c5f8216fc5c52f3e488fca
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-05-03 14:37:00 +0100

    chore: imple home page

--- mobile/App.js
+++ mobile/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import "./global.css";
 import theme from "./theme.json";
 import { Header } from "./components/Header";
@@ -11,28 +11,35 @@ export default function App() {
         height: "100%",
       }}
     >
-      <Header/>
+      <Header />
       <View
         style={{
           height: "20%",
           width: "100%",
           backgroundColor: theme.app[50],
-          padding:20
+          padding: 20,
         }}
       >
         <View
           style={{
             flex: 1,
-            alignItems: "center",
+            alignItems: "flex-end",
             flexDirection: "row",
             justifyContent: "space-between",
           }}
         >
-       
+          <Text>Image</Text>
+          <Text>Video</Text>
+          <Text>Audio</Text>
+          <Text>Document</Text>
         </View>
       </View>
+
+      <Button
+        title=""
+        style={{ backgroundColor: theme.app[600], width: 20, height: 20 }}
+      ></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
-
