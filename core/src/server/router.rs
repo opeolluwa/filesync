5fe531c7191de3b8ad949bc14d55dcdbbbb22273
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-05-10 11:22:00 +0100

    chore

--- core/src/server/router.rs
+++ core/src/server/router.rs
@@ -6,14 +6,18 @@ use axum::{
 };
 
 use super::routes::{
-    accept_file_upload, download_file, file_upload_form, get_file, health_check, ping_server, system_information
+    accept_file_upload, download_file, file_upload_form_new, get_file, health_check, ping_server,
+    system_information,
 };
 
 // the app is moved here to allow sharing across test modules
 pub fn app() -> Router {
     Router::new()
         .route("/", get(ping_server))
-        .route("/upload", post(accept_file_upload).get(file_upload_form))
+        .route(
+            "/upload",
+            post(accept_file_upload).get(file_upload_form_new),
+        )
         .route("/health", post(accept_file_upload).get(health_check))
         .route("/api/sys-info", get(system_information))
         .route("/api/download", get(download_file))
