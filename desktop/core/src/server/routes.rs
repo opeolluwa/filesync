98c2263919da0b98fccbc251c7fd17477b8ad0d4
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-06-21 08:26:37 +0100

    feat: impl websockets

--- desktop/core/src/server/routes.rs
+++ desktop/core/src/server/routes.rs
@@ -1,29 +1,27 @@
 use axum::body::{Body, Bytes};
-use axum::extract::ws::{CloseFrame, Message, WebSocket};
-use axum::extract::{connect_info::ConnectInfo, Multipart, Query, WebSocketUpgrade};
+// use axum::extract::ws::{Message, WebSocket};
+use axum::extract::{Multipart, Query};
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::{BoxError, Json};
-use axum_extra::TypedHeader;
 use futures::stream::{Stream, TryStreamExt};
 
-use futures::{SinkExt, StreamExt};
+use axum_typed_websockets::{Message, WebSocket, WebSocketUpgrade};
 use http::header;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use serde_json::Value;
-use std::borrow::Cow;
 use std::fs;
-use std::net::SocketAddr;
 use std::ops::ControlFlow;
 use std::path::PathBuf;
 use tokio_util::io::{ReaderStream, StreamReader};
 
-use tokio::fs::File;
-use tokio::io::{self, BufWriter};
+use crate::context::websockets::{SocketMessage as ClientMessage, SocketMessage as ServerMessage};
 
 use crate::utils::{system_info::SystemInformation, CommandData};
 use crate::UPLOAD_DIRECTORY;
+use tokio::fs::File;
+use tokio::io::{self, BufWriter};
 #[derive(Debug, Serialize, Deserialize)]
 
 /// destructure query parameter
@@ -191,166 +189,35 @@ pub async fn get_file(Query(QueryParams { file_path }): Query<QueryParams>) -> i
 }
 
 /// send and recieve websocket connection from peer
-pub async fn notify_peer(
-    ws: WebSocketUpgrade,
-    user_agent: Option<TypedHeader<headers::UserAgent>>,
-    ConnectInfo(addr): ConnectInfo<SocketAddr>,
-) -> impl IntoResponse {
-    let user_agent = if let Some(TypedHeader(user_agent)) = user_agent {
-        user_agent.to_string()
-    } else {
-        String::from("Unknown browser")
-    };
-    println!("`{user_agent}` at {addr} connected.");
-    // finalize the upgrade process by returning upgrade callback.
-    // we can customize the callback by sending additional info such as address.
-    ws.on_upgrade(move |socket| handle_socket(socket, addr))
+pub async fn notify_peer(ws: WebSocketUpgrade<ServerMessage, ClientMessage>) -> impl IntoResponse {
+    ws.on_upgrade(handle_socket)
 }
 
-/// Actual websocket statemachine (one will be spawned per connection)
-async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
-    //send a ping (unsupported by some browsers) just to kick things off and get a response
-    if socket.send(Message::Ping(vec![1, 2, 3])).await.is_ok() {
-        println!("Pinged {who}...");
-    } else {
-        println!("Could not send ping {who}!");
-        // no Error here since the only thing we can do is to close the connection.
-        // If we can not send messages, there is no way to salvage the statemachine anyway.
-        return;
-    }
+async fn handle_socket(mut socket: WebSocket<ServerMessage, ClientMessage>) {
+    socket
+        .send(Message::Item(ServerMessage::new(
+            "test message from server",
+        )))
+        .await
+        .ok();
 
-    // receive single message from a client (we can either receive or send with socket).
-    // this will likely be the Pong for our Ping or a hello message from client.
-    // waiting for message from a client will block this task, but will not block other client's
-    // connections.
     if let Some(msg) = socket.recv().await {
-        if let Ok(msg) = msg {
-            if process_message(msg, who).is_break() {
-                return;
+        match msg {
+            // Ok(Message::Item(client_message)) => {
+            //     println!("new message from client: {:?}", client_message);
+            // }
+            Ok(client_message) => {
+                println!("new message from client: {:?}", client_message);
             }
-        } else {
-            println!("client {who} abruptly disconnected");
-            return;
+            Err(err) => {
+                eprintln!("got error: {}", err);
+            }
         }
     }
 
-    // Since each client gets individual statemachine, we can pause handling
-    // when necessary to wait for some external event (in this case illustrated by sleeping).
-    // Waiting for this client to finish getting its greetings does not prevent other clients from
-    // connecting to server and receiving their greetings.
-    for i in 1..5 {
-        if socket
-            .send(Message::Text(format!("Hi {i} times!")))
-            .await
-            .is_err()
-        {
-            println!("client {who} abruptly disconnected");
-            return;
-        }
-        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-    }
-
-    // By splitting socket we can send and receive at the same time. In this example we will send
-    // unsolicited messages to client based on some sort of server's internal event (i.e .timer).
-    let (mut sender, mut receiver) = socket.split();
-
-    // Spawn a task that will push several messages to the client (does not matter what client does)
-    let mut send_task = tokio::spawn(async move {
-        let n_msg = 20;
-        for i in 0..n_msg {
-            // In case of any websocket error, we exit.
-            if sender
-                .send(Message::Text(format!("Server message {i} ...")))
-                .await
-                .is_err()
-            {
-                return i;
-            }
-
-            tokio::time::sleep(std::time::Duration::from_millis(300)).await;
-        }
-
-        println!("Sending close to {who}...");
-        if let Err(e) = sender
-            .send(Message::Close(Some(CloseFrame {
-                code: axum::extract::ws::close_code::NORMAL,
-                reason: Cow::from("Goodbye"),
-            })))
-            .await
-        {
-            println!("Could not send Close due to {e}, probably it is ok?");
-        }
-        n_msg
-    });
-
-    // This second task will receive messages from client and print them on server console
-    let mut recv_task = tokio::spawn(async move {
-        let mut cnt = 0;
-        while let Some(Ok(msg)) = receiver.next().await {
-            cnt += 1;
-            // print message and break if instructed to do so
-            if process_message(msg, who).is_break() {
-                break;
-            }
-        }
-        cnt
-    });
-
-    // If any one of the tasks exit, abort the other.
-    tokio::select! {
-        rv_a = (&mut send_task) => {
-            match rv_a {
-                Ok(a) => println!("{a} messages sent to {who}"),
-                Err(a) => println!("Error sending messages {a:?}")
-            }
-            recv_task.abort();
-        },
-        rv_b = (&mut recv_task) => {
-            match rv_b {
-                Ok(b) => println!("Received {b} messages"),
-                Err(b) => println!("Error receiving messages {b:?}")
-            }
-            send_task.abort();
-        }
-    }
-
-    // returning from the handler closes the websocket connection
-    println!("Websocket context {who} destroyed");
 }
 
-/// helper to print contents of messages to stdout. Has special treatment for Close.
-fn process_message(msg: Message, who: SocketAddr) -> ControlFlow<(), ()> {
-    match msg {
-        Message::Text(t) => {
-            println!(">>> {who} sent str: {t:?}");
-        }
-        Message::Binary(d) => {
-            println!(">>> {} sent {} bytes: {:?}", who, d.len(), d);
-        }
-        Message::Close(c) => {
-            if let Some(cf) = c {
-                println!(
-                    ">>> {} sent close with code {} and reason `{}`",
-                    who, cf.code, cf.reason
-                );
-            } else {
-                println!(">>> {who} somehow sent close message without CloseFrame");
-            }
-            return ControlFlow::Break(());
-        }
 
-        Message::Pong(v) => {
-            println!(">>> {who} sent pong with {v:?}");
-        }
-        // You should never need to manually handle Message::Ping, as axum's websocket library
-        // will do so for you automagically by replying with Pong and copying the v according to
-        // spec. But if you need the contents of the pings you can see them here.
-        Message::Ping(v) => {
-            println!(">>> {who} sent ping with {v:?}");
-        }
-    }
-    ControlFlow::Continue(())
-}
 #[cfg(test)]
 mod basic_endpoints {
     use crate::server::router;
