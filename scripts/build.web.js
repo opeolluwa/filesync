e8635929556a8050e5cdb6fb6ea3f032b671f63c
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-08-28 14:47:32 +0100

    chore

--- scripts/build.web.js
+++
@@ -1,9 +0,0 @@
-// build the web static assets and export
-
-import shell from "shelljs";
-
-import { log_success } from "./utils.scripts.js";
-
-shell.exec("cd web && npm run export")
-
-log_success("Web static files exported successfully!")
--- scripts/copy_codec.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash 
-
-
-cp -r codec out/render
