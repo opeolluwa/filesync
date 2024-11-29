2272bbe69b0f6104457a4cd697d4790770d63046
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-11-29 02:39:15 +0100

    install andt

---
+++ src/theme/themeConfig.ts
@@ -0,0 +1,11 @@
+// theme/themeConfig.ts
+import type { ThemeConfig } from "antd";
+
+const theme: ThemeConfig = {
+  token: {
+    fontSize: 16,
+    colorPrimary: "#3074F5",
+  },
+};
+
+export default theme;
