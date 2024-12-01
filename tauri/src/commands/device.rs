27642bdcb82862866ebbddefda49b9b0aa652afb
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-12-01 17:14:46 +0100

    refactor: remove deadcode

--- tauri/src/commands/device.rs
+++
@@ -1,9 +0,0 @@
-use device::device_information::Device;
-
-use crate::CommandData;
-
-#[tauri::command]
-pub fn get_device_information() -> CommandData<Device> {
-    let device_information = Device::new();
-    CommandData::new(device_information)
-}
