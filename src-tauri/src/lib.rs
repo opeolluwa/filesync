97233daa16265ccfa3a23dc0a981ce0df7367084
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-03-11 16:37:39 +0100

    move for barcode scanner

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -17,6 +17,7 @@ pub fn run() {
     }];
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(
             tauri_plugin_sql::Builder::default()
                 .add_migrations("sqlite:filesync.db", migrations)
